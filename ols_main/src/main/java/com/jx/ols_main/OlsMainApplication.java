bde82ec1a5680a93870a87dd5a72da7952ae8fcb
wangkezheng <191524234>
2019-12-24 10:39:14 +0800

    项目初试框架

---
+++ ols_main/src/main/java/com/jx/ols_main/OlsMainApplication.java
@@ -0,0 +1,13 @@
+package com.jx.ols_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlsMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlsMainApplication.class, args);
+    }
+
+}
---
+++ ols_main/src/test/java/com/jx/ols_main/OlsMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jx.ols_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlsMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
