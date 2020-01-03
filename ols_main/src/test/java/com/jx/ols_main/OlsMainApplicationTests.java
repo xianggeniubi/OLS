6c174ee3065292dab38951ca2291546f6772f9a4
wangkezheng <191524234>
2020-01-03 23:29:15 +0800

    日志框架搭建

--- ols_main/src/test/java/com/jx/ols_main/OlsMainApplicationTests.java
+++ ols_main/src/test/java/com/jx/ols_main/OlsMainApplicationTests.java
@@ -1,13 +1,17 @@
 package com.jx.ols_main;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OlsMainApplicationTests {
 
+    Log logger = LogFactory.getLog(OlsMainApplicationTests.class);
     @Test
     void contextLoads() {
+        logger.info("日志已搞定");
     }
 
 }
